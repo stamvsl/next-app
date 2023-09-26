bbcf9c1fd72f0e31bdd23f93176300fc2c9c9b4a
stamvsl <stam.vsl@gmail.com>
2023-09-26 18:09:16 +0300

    Navbar component

---
+++ next-app/src/pages/components/navbar.js
@@ -0,0 +1,73 @@
+
+import {
+    Box,
+    Flex,
+    Text,
+    Button,
+    Stack,
+    useColorModeValue,
+    Spacer,
+} from '@chakra-ui/react'
+
+export default function Navbar(){
+    return(
+        <Box >
+        <Flex
+          bg="orange.300"
+         
+          minH={'60px'}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          align={'center'}>
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}>
+            
+          </Flex >
+            
+         
+       
+          
+            <Box>
+            <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}  color={'white'} mr="6">
+              Κινήσεις
+            </Button>
+            <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}  color={'white'} mr="6">
+              Συναλλασσόμενοι
+            </Button>
+            <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}  color={'white'} mr="6">
+             Εκτυπώσεις
+            </Button>
+            </Box>
+            <Spacer />
+
+            <Box>
+
+            <Button
+           as={'a'}
+           display={{ base: 'none', md: 'inline-flex' }}
+           fontSize={'sm'}
+           fontWeight={600}
+           color={'white'}
+           bg={'orange.500'}
+           href={'#'}
+           _hover={{
+            bg: 'orange.700',
+           }}>
+           Έξοδος
+         </Button>
+         </Box>
+
+         
+          
+        </Flex>
+          
+        
+      </Box>
+    )
+  }
+    
