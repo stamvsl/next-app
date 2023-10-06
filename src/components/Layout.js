a63eb816bc25c775edc5202f2ccc2e8da98fb050
Manos Markou <markou_13@yahoo.com>
2023-10-06 17:28:18 +0300

    Layout, container

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Container } from "@chakra-ui/react";
 
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <Box p="4px" bg="orange.100">
-      <Navbar />
-      {children}
-    </Box>
+    <Container minHeight="100vh" minWidth="100%" p="4px" bg="orange.100">
+      <Box>
+        <Navbar />
+      </Box>
+      <Box>{children}</Box>
+    </Container>
   );
 }
