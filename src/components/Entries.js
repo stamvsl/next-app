aec387710337ad58aed801e16a45719ed09a896d
stamvsl <stam.vsl@gmail.com>
2023-10-30 11:28:23 +0200

    calculate VAT

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import {
+  Flex,
   Radio,
   RadioGroup,
   Stack,
@@ -7,13 +9,209 @@ import {
   HStack,
   VStack,
   Box,
-  Flex,
+  FormControl,
+  FormLabel,
+  Select,
 } from "@chakra-ui/react";
 
+const initialvalues = {
+  date: "",
+  number: "",
+  transactor: "",
+  description: "",
+  netValue: "",
+  vatClass: "0.24",
+  vatValue: "",
+  grossValue: "",
+  comments: "",
+};
+
+const calculateNewState = (formData, name, value) => {
+  return {
+    ...formData,
+    [name]: value,
+  };
+};
+
 export default function Entries() {
+  const [formData, setFormData] = useState(initialvalues);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(`
+    Date: ${formData.date}
+    Number: ${formData.number}
+    transactor: ${formData.transactor}
+    description: ${formData.description}
+    netValue: ${formData.netValue}
+    vatClass: ${formData.vatClass}%
+    vatValue: ${formData.vatValue}
+    grossValue: ${formData.grossValue}
+    comments: ${formData.comments}
+    `);
+  };
+
+  // const handleChange = (e) => {
+  //   const newState = calculateNewState(formData, e.target.name, e.target.value);
+  //   newState.vatValue = newState.netValue * newState.vatClass;
+  //   newState.grossValue =
+  //     parseFloat(newState.netValue) + parseFloat(newState.vatValue);
+  //   setFormData(newState);
+  // };
+
+  const handleChange = (e) => {
+    const newState = calculateNewState(formData, e.target.name, e.target.value);
+
+    if (e.target.name === "vatClass") {
+      const vatClass = parseFloat(e.target.value);
+      const netValue = parseFloat(newState.netValue);
+      const vatValue = (vatClass / 100) * netValue;
+      newState.vatValue = vatValue;
+      newState.grossValue = netValue + vatValue;
+    }
+
+    setFormData(newState);
+  };
+
   return (
     <Box p={{ base: "12px", sm: "25px" }}>
-      <RadioGroup defaultValue="2">
+      <Flex flexDirection="column">
+        <FormControl>
+          <HStack>
+            <FormLabel>Ημερομηνία</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+        <FormControl>
+          <HStack>
+            <FormLabel>Αριθμός</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="number"
+              value={formData.number}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+        <FormControl>
+          <HStack>
+            <FormLabel>Συναλλασσόμενος</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="transactor"
+              value={formData.transactor}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Περιγραφή</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Καθαρή αξία</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="netValue"
+              value={formData.netValue}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Κατηγορία ΦΠΑ</FormLabel>
+            <Select
+              placeholder=">Κατηγορία ΦΠΑ"
+              borderColor="orange.500"
+              name="vatClass"
+              onChange={handleChange}
+              defaultValue={"24"}
+            >
+              <option value="0">0%</option>
+              <option value="6">6%</option>
+              <option value="13">13%</option>
+              <option value="17">17%</option>
+              <option value="24">24%</option>
+            </Select>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Αξία ΦΠΑ αξία</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="vatValue"
+              value={formData.vatValue}
+              onChange={(e) => {
+                handleChange(e);
+                calculateVatValue(e);
+              }}
+            ></Input>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Μικτή αξία</FormLabel>
+            <Input
+              type="text"
+              borderColor="orange.500"
+              name="grossValue"
+              value={formData.grossValue}
+              onChange={handleChange}
+            ></Input>
+          </HStack>
+        </FormControl>
+
+        <FormControl>
+          <HStack>
+            <FormLabel>Σχόλια</FormLabel>
+            <textarea
+              type="text"
+              borderColor="orange.500"
+              name="comments"
+              value={formData.comments}
+              onChange={handleChange}
+            ></textarea>
+          </HStack>
+        </FormControl>
+
+        <Button onClick={handleClick} type="submit">
+          Καταχώριση
+        </Button>
+      </Flex>
+    </Box>
+  );
+}
+
+// direction={{ md: "row", base: "column" }}>
+
+{
+  /* <RadioGroup defaultValue="2">
         <HStack spacing={5}>
           <Radio value="1" colorScheme="orange">
             Έσοδα
@@ -22,70 +220,5 @@ export default function Entries() {
             Έξοδα
           </Radio>
         </HStack>
-      </RadioGroup>
-
-      <Stack m={"10"} direction={{ md: "row", base: "column" }}>
-        <label>Συναλλασσόμενος</label>
-        <Input
-          placeholder="Όνομα, ΑΦΜ"
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-        />
-        <Button background={"orange.300"}>Search</Button>
-      </Stack>
-
-      <Stack m={"10"} direction={{ md: "row", base: "column" }}>
-        <label>Ημερομηνία</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-          w={"100"}
-        />
-        <label>Αριθμός</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-          w={"100"}
-        />
-        <label>Περιγραφή</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-        />
-      </Stack>
-
-      <Stack m={"10"} direction={{ md: "row", base: "column" }}>
-        <label>Καθαρή αξία</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-        />
-        <label>Συντελεστής ΦΠΑ</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-          w={"50"}
-        />
-        <label>ΦΠΑ</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-        />
-        <label>Μικτή Αξία</label>
-        <Input
-          borderColor={"orange.300"}
-          focusBorderColor="orange.300"
-          borderWidth="2px"
-        />
-      </Stack>
-      <Button background={"orange.300"}>Υποβολή</Button>
-    </Box>
-  );
+      </RadioGroup> */
 }
