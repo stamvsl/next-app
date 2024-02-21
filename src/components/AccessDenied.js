e8a9d4508010d34688292638ca104c2b4549184c
stamvsl <stam.vsl@gmail.com>
2024-02-21 16:07:31 +0200

    Changes for deployment

--- src/components/AccessDenied.js
+++ src/components/AccessDenied.js
@@ -5,15 +5,15 @@ export default function AccessDenied() {
     <>
       <h1>Access Denied</h1>
       <p>
-        <a
-          href="/auth/signin"
+        <Link
+          to="/auth/signin"
           onClick={(e) => {
             e.preventDefault();
             signIn();
           }}
         >
           You must be signed in to view this page
-        </a>
+        </Link>
       </p>
     </>
   );
--- src/pages/prints/index.js
+++ src/pages/prints/index.js
@@ -2,7 +2,7 @@ import Prints from "@/components/Prints";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
-export default function prints() {
+export default function Prints() {
   const { status } = useSession();
 
   const router = useRouter();
