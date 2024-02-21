9e4e6d9d6e5ad61efdc7a1fe54970994db260afa
stamvsl <stam.vsl@gmail.com>
2024-02-21 16:36:59 +0200

    Fixed main link

--- src/pages/entries/index.js
+++ src/pages/entries/index.js
@@ -1,7 +1,5 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { Input, Container, Text } from "@chakra-ui/react";
 import Entries from "@/components/Entries";
 
 export default function EntriesPage() {
